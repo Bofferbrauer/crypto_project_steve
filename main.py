558a18af187fc840035c8e5a5cbc1f49aa8fc24b
Steve Weidig <75318450+Bofferbrauer@users.noreply.github.com>
2022-06-12 13:23:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from crypto.app import App
 # events_list = ["TestTestTest123"]
 # senders_list = ["Kosmas"]
 
+# Getting the message from the sender
 mock_senders = [Mocksender()]
 # mock_senders.send(events_list[0])
+
+# Getting the event linked to the message
 events = [MockEvent(mock_senders)]
 
 # Taking the message from the event and sets the time interval at which it will be updated/published
-app = App(events, 5)
+app = App(events, 5) # ! Since there is no stop condition anywhere, this will loop indefinitely and print the mock message every 5 seconds. Type Ctrl+C repeatedly to make VSCode stop the program.
 
 app.start()
 
