c23ebd01b503e39ffb81bcb6ef6adad69a520106
Steve Weidig <75318450+Bofferbrauer@users.noreply.github.com>
2022-06-14 11:08:26 +0200

    validator

--- crypto/data/source.py
+++ crypto/data/source.py
@@ -1,17 +1,38 @@
-from pydantic import BaseModel, PositiveFloat
+from pydantic import BaseModel, PositiveFloat, ValidationError, root_validator
 from typing import List, Dict
 
+# Setting up the values and making sure they're positive float numbers
 class OrderBook(BaseModel):
     bid: PositiveFloat
     ask: PositiveFloat
     bid_volume: PositiveFloat
     ask_volume: PositiveFloat
 
-def mid(bid, ask_price) -> List[PositiveFloat]:
-    return((bid+ask_price)/2)
+    # Validating the results and throwing an error if an entry is not a positive float
+    @root_validator
+    def check_price(cls,values):
+        bid_x = values.get("bid")
+        ask_x = values.get("ask")
 
-test = OrderBook(bid=2.3, ask=3.6, bid_volume=8, ask_volume=2.3)
+        #making sure that the asking price is higher than the bid
+        if bid_x <= ask_x:
+            return values
+        else:
+            raise ValueError("The asking price is too high")
+            
+    def mid(bid, ask) -> List[PositiveFloat]:
+        return((bid+ask)/2)
 
-print(f" The price paid is {test.bid}, the asking price was {test.ask}, wanted to buy {test.bid_volume} and got {test.ask_volume}." )
+try:
+    test = OrderBook(bid=2.3, ask=3.6, bid_volume=8, ask_volume=2.3)
+    print(f" The price paid is {test.bid}, the asking price was {test.ask}, wanted to buy {test.bid_volume} and got {test.ask_volume}." )
+    test2 = OrderBook(bid=2.3, ask=1.6, bid_volume=8, ask_volume=2.3)
+    print(f" The price paid is {test2.bid}, the asking price was {test2.ask}, wanted to buy {test2.bid_volume} and got {test2.ask_volume}." )
+
+except ValueError as e:
+    print(e)
+
+
+
 
 
