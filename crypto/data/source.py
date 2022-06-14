9550896cdffb4c48fd024f20d4d271324b786695
Steve Weidig <75318450+Bofferbrauer@users.noreply.github.com>
2022-06-14 09:54:35 +0200

    Update source.py

--- crypto/data/source.py
+++ crypto/data/source.py
@@ -1,5 +1,17 @@
-from pydantic import Basemodel
+from pydantic import BaseModel, PositiveFloat
 from typing import List, Dict
 
+class OrderBook(BaseModel):
+    bid: PositiveFloat
+    ask: PositiveFloat
+    bid_volume: PositiveFloat
+    ask_volume: PositiveFloat
+
+def mid(bid, ask_price) -> List[PositiveFloat]:
+    return((bid+ask_price)/2)
+
+test = OrderBook(bid=2.3, ask=3.6, bid_volume=8, ask_volume=2.3)
+
+print(f" The price paid is {test.bid}, the asking price was {test.ask}, wanted to buy {test.bid_volume} and got {test.ask_volume}." )
 
 
